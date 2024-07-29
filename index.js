eb08995e2aa79b9b5ec789f8b1e494c39b485621
Rani Hirani <118107407+ranixhirani@users.noreply.github.com>
2024-07-29 16:30:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-//var generateName = require("sillyname");
-
-/*import generateName from "sillyname";
-var sillyName = generateName();
-console.log(`My name is ${sillyName}.`);
-
-/*import superheroes from "superheroes";
-const name = superheroes.random();
-console.log(`I am ${name}!`);*/
-
 import inquirer from 'inquirer';
 import qr from "qr-image";
 import fs from "fs";
